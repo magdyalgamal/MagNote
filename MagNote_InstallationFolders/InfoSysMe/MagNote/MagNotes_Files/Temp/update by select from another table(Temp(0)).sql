aa63b9ad7f028b6d005cfde5dced07d550f0640a
Magdy AlGamal <magdyalgamal@gmail.com>
2024-08-27 08:54:43 +0300

    deleting installation folder

--- MagNote_InstallationFolders/InfoSysMe/MagNote/MagNotes_Files/Temp/update by select from another table(Temp(0)).sql
+++
@@ -1,5 +0,0 @@
-UPDATE Sales_Invoices 
-SET     Invoice_Customer_No = (SELECT  Customers.Customer_No 
-                          FROM   Customers 
-                          WHERE  Customers.Customer_Name = 
-                                                Sales_Invoices.Invoice_Customer_Name) 
